66c272b74f26776cf1b8a9f6c1f0750dbbbaae06
dragun0 <l.dragun.14@aberdeen.ac.uk>
2021-06-05 18:06:50 +0200

    Create UserDataBaseFunctions.py

---
+++ TEST/UserDataBaseFunctions.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 17:24:28 2021
+
+@author: leoni
+"""
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g
+from psycopg2 import connect
+from werkzeug.exceptions import abort
+
+
+
+#This function creates a connection to the database for the tables created
+def conn_db():
+    if 'db' not in g:
+        DBfile = open('Database/dbConfig.txt')
+        connection = DBfile.readline()
+        g.db =  connect(connection)
+    return g.db
+
+#This function closes the connection to the database
+def enddb_conn():
+    if 'db' in g:
+        g.db.close()
+        g.pop('db')
+        
+
+def mysession():
+    userid = session.get('userid')
+    if userid is None:
+        g.user = None
+    else:
+        conn = conn_db()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM sys_table WHERE userid = %s', (userid,))
+        g.user = cur.fetchone()
+        cur.close()
+        conn.commit()
+    if g.user is not None:
+        return True
+    else: 
+        return False
+
+
+def get_post(id):
+    conn = conn_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM post WHERE post.post_id = %s", (id,))
+    post = cur.fetchone()
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+    if post[1] != g.user[0]:
+        abort(403)
+    return post
